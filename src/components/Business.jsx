138cf16e43c58343864e46f38d76eaea2e2bf0bb
Bryan4real <mwiruki@outlook.com>
2023-04-10 13:55:57 +0300

    business component debuging

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -6,7 +6,7 @@ const FeatureCard = ({ icon, title, content, index}) => (
 
   <div className={`flex flex-row p-6 rounded-[20px] ${index !== features.length - 1 ? "mb-6" : "mb-0"} feature-card`}>
     <div className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}>
-      <img src={icon} alt="icon" className="w-[50%] h-[50%] object-contain"/>
+      <img src={icon} alt="star" className="w-[50%] h-[50%] object-contain"/>
     </div>
     <div className="flex-1 flex flex-col ml-3">
         <h4 className="font-poppins font-semibold text-white text-[18px] leading-[23px] mb-1">
@@ -32,11 +32,11 @@ const Business = () => {
             earning rewards and saving money. But with hundreds of 
             credit cards on the market.</p>
 
-            <Button styles="mt-10"/>
+            <Button styles={`mt-10`}/>
 
       </div>
 
-      <div className={`${layout.sectionImg} flex-col}`}>
+      <div className={`${layout.sectionImg} flex-col`}>
         {features.map((feature, index) => (
           <FeatureCard  key={feature.id} {...feature} index={index} />
           ))}
