b21dd33b81ae1f2dc3bcce3f575f626fae49d6a7
Bryan4real <mwiruki@outlook.com>
2023-04-10 14:24:35 +0300

    cardDeals section implementation

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import { card } from "../assets"; 
+import styles, {layout} from "../style";
+import Button from "./Button";
+
+
 
 const CardDeal = () => {
   return (
-    <div>CardDeal</div>
+    <section className={layout.section}>
+        <div className={layout.sectionInfo}>
+          <h2 className={styles.heading2}>
+            Find a better card deal <br className="sm:block hidden"/> In afew steps. 
+          </h2>
+          <p className={`${styles.paragraph} max-w-[470px]} mt-5`}>
+          fully responsive, expertly crafted component examples you can drop into your 
+          Tailwind projects and customize to your heart’s content.
+          </p>
+          <Button className="mt-10"/>
+        </div>
+        <div className={layout.sectionImg}>
+          <img src={card} alt="card" className="w-[100%] h-[100%]"/>
+        </div>
+    </section>
   )
 }
 
